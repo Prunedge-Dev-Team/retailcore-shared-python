0fa81fa0608f2e5fbbc8014d01d53df77246f544
sodiqchinko <sodiqridwan100@gmail.com>
2022-10-25 14:28:03 +0100

    include package data

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 import os
 with open("README.md", "r") as fh:
     description = fh.read()
-# github_token = os.environ['GITHUB_TOKEN']
+github_token = os.environ['GITHUB_TOKEN']
 
 setuptools.setup(
     name="sterling-utils",
@@ -13,7 +13,8 @@ setuptools.setup(
     description="A utilty package for sterling core",
     long_description=description,
     long_description_content_type="text/markdown",
-    # url=f"git+https://{github_token}@github.com/Prunedge-Dev-Team/retailcore-shared-python.git",
+    include_package_data=True,
+    url=f"git+https://{github_token}@github.com/Prunedge-Dev-Team/retailcore-shared-python.git",
     license='MIT',
     python_requires='>=3.8',
     install_requires=[]
