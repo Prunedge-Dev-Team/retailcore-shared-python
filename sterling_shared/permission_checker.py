b8a0f4330cd11af7d523b37fad5ccb81a727d42d
sodiqchinko <sodiqridwan100@gmail.com>
2022-10-25 16:19:07 +0100

    update permission mixin

--- sterling_shared/permission_checker.py
+++ sterling_shared/permission_checker.py
@@ -49,14 +49,16 @@ class UserAuthMixin:
         try:
             print(auth_decode_url)
             res = requests.post(auth_decode_url, data=data, headers=headers)
-            user_data = json.loads(res.text)
-            request.user = UserData(user_data)
+
         except requests.ConnectionError as err:
             raise serializers.ValidationError(f"Cannot establish connection: {err}")
         except requests.HTTPError as err:
             raise serializers.ValidationError(f"HTTP Error: {err}")
         except Exception as err:
             raise serializers.ValidationError(f"Error occurred: {err}")
+        print(res.text)
+        user_data = json.loads(res.text)
+        request.user = UserData(user_data)
 
 
 class PermissionMixin:
