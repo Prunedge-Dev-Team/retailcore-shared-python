63fe456cb5ef0732ac74bb71ee6ab58001f57fa7
sodiqchinko <sodiqridwan100@gmail.com>
2022-10-25 14:05:20 +0100

    remove required token to test

--- setup.py
+++ setup.py
@@ -1,18 +1,19 @@
 import setuptools
-
+import os
 with open("README.md", "r") as fh:
     description = fh.read()
+# github_token = os.environ['GITHUB_TOKEN']
 
 setuptools.setup(
     name="sterling-utils",
     version="0.0.1",
-    author="Sodiq Ridwan",
+    author="Ridwan Sodiq",
     author_email="daniel.ale@prunedge.com",
     packages=["sterling_shared"],
     description="A utilty package for sterling core",
     long_description=description,
     long_description_content_type="text/markdown",
-    url="https://github.com/Prunedge-Dev-Team/retailcore-shared-python",
+    # url=f"git+https://{github_token}@github.com/Prunedge-Dev-Team/retailcore-shared-python.git",
     license='MIT',
     python_requires='>=3.8',
     install_requires=[]
