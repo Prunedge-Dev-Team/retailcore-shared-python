9ef9758eadd8f841a9d694b06c5a66a12211594a
pascal-storri <pascal@storri.ai>
2023-11-16 12:43:46 +0100

    service to service auth

--- sterling_shared/permission_checker.py
+++ sterling_shared/permission_checker.py
@@ -1,4 +1,8 @@
+import json
+import os
+
 from rest_framework.exceptions import PermissionDenied, AuthenticationFailed
+import requests
 
 
 def get_user_permissions(user):
@@ -10,7 +14,8 @@ def get_user_permissions(user):
     raise AuthenticationFailed
 
 
-def check_user_has_permissions(user, perms):
+def check_user_has_permissions(request, perms, auth_service_url):
+    user = request.user
     """
     Function to check if a user has any of the permissions passed.
     """
@@ -20,16 +25,29 @@ def check_user_has_permissions(user, perms):
         return any(_perm in perm_list for _perm in perms)
 
     if user.is_admin is False and perms and check_perm(user_permissions) is False:
+        if 'X-Header-Service-Auth' in request.headers and auth_service_url:
+            params = request.GET if request.method == 'GET' else request.POST
+            third_party_url = auth_service_url + "/api/v1/service-auth/verify-header-key/"
+            json_payload = json.dumps({'api_key': request.headers.get('X-Header-Service-Auth')})
+            headers = {
+                'Content-Type': 'application/json',
+                'Authorization': request.headers.get('Authorization'),
+            }
+            response = requests.get(third_party_url, data=json_payload, headers=headers, params=params)
+            if response.status_code == 200 and response.json()['success']:
+                return True
         raise PermissionDenied
+
 
 class PermissionMixin:
     """
     Custom Permission mixin
     """
     custom_permissions = None
+    auth_service_url = None
 
     def check_permissions(self, request):
-        check_user_has_permissions(request.user, self.get_custom_permissions())
+        check_user_has_permissions(request, self.get_custom_permissions(), self.auth_service_url)
         return super().check_permissions(request)
 
     def get_custom_permissions(self):
